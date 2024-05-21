445327c65459299f6373e4851aefd2ed6091ba54
Enoch Osarenren <osarenrenenoch@gmail.com>
2024-05-21 12:12:35 +0100

    deploy nestjs app to vercel using github actions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Deployed NestJs app to Vercel using Github Actions!!!';
   }
 }
